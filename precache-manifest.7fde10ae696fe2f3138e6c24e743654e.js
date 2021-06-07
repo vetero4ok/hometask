0ee5d35535ac6abc7470f947bf02910b177a71a2
vetero4ok <82808215+vetero4ok@users.noreply.github.com>
2021-06-07 18:23:07 +0300

    Updates

---
+++ precache-manifest.7fde10ae696fe2f3138e6c24e743654e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69a4f19b4507eef3c5aa8f9aff84922e",
+    "url": "/hometask/index.html"
+  },
+  {
+    "revision": "398e09a370b337a1526b",
+    "url": "/hometask/static/css/main.37b83679.chunk.css"
+  },
+  {
+    "revision": "a5eec3b31ca7866aef3a",
+    "url": "/hometask/static/js/2.490fbe74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hometask/static/js/2.490fbe74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "398e09a370b337a1526b",
+    "url": "/hometask/static/js/main.4b7c39aa.chunk.js"
+  },
+  {
+    "revision": "4243c4059d9baed79606",
+    "url": "/hometask/static/js/runtime-main.756bc61c.js"
+  }
+]);
